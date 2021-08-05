e1d723474dfdfd821d1498c25f65b616a616fa95
Nicholas Garcia <ncgarcia@mit.edu>
2021-08-05 17:55:55 +0000

    Change test name to be more verbose

--- src/test/java/com/google/finapp/FinAppIT.java
+++ src/test/java/com/google/finapp/FinAppIT.java
@@ -71,7 +71,7 @@ public class FinAppIT {
   }
 
   @Test
-  public void testBasicCreateAccount() throws Exception {
+  public void createAccount_createsSingleValidAccount() throws Exception {
     for (SpannerDaoInterface spannerDao : List.of(daoJava, daoJDBC)) {
       ByteArray accountId = UuidConverter.getBytesFromUuid(UUID.randomUUID());
       BigDecimal bigDecimalTwo = new BigDecimal(2);
