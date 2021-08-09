303f0dc3e7ca64ecd05cb6dca9b2ac51c914094b
Nicholas Garcia <ncgarcia@mit.edu>
2021-08-09 10:24:17 -0700

    Use IntegrationTestEnv (#28)
    This commit introduces IntegrationTestEnv into this app's tests. This testing environment is used by both java-spanner and java-spanner-jdbc (and possibly other repos) to set up an integration test environment. This includes initializing a test database and deleting it at the end of tests. Tests can be ran with `mvn integration-test`.
    
    We also get rid of the explicit assignment of variables for project, instance, and database ID, instead getting these variables from IntegrationTestEnv, which is influenced by both the environment in which the tests are run (including general gcloud setup) and the config in the Maven pom.xml file.
    
    A lot of the decisions made in this commit were influenced by previous uses of IntegrationTestEnv and this pom.xml file.
    
    Another part of the commit is the removal of parametrized tests:
    
    ParameterizedTest tests are run before the BeforeClass methods. Before, I had used a method (suggested in a related Stack Overflow forum) to get around this by replacing my BeforeClass methods with a simple static {}. However, this had some strange interactions with the ClassRule (which was introduced with IntegrationTestEnv).
    
    Switching the static back to BeforeClass to ensure the order of ClassRule to BeforeClass to Before to tests, I had to rework the code to no longer use ParameterizedTest, instead opting to set the SpannerDaoInterface implementation through environment.
    
    By default, the tests use the Java client implementation, but adding `-DSPANNER_USE_JDBC=true` to `mvn integration test` switches to the JDBC implementation.

--- src/test/java/com/google/finapp/FinAppIT.java
+++ src/test/java/com/google/finapp/FinAppIT.java
@@ -16,60 +16,97 @@
 
 package com.google.finapp;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.google.common.truth.Truth.assertThat;
 
 import com.google.cloud.ByteArray;
+import com.google.cloud.spanner.Database;
 import com.google.cloud.spanner.DatabaseClient;
-import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.IntegrationTest;
+import com.google.cloud.spanner.IntegrationTestEnv;
 import com.google.cloud.spanner.Key;
 import com.google.cloud.spanner.KeySet;
 import com.google.cloud.spanner.ResultSet;
-import com.google.cloud.spanner.Spanner;
-import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.spanner.testing.RemoteSpannerHelper;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 import java.util.UUID;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+@Category(IntegrationTest.class)
 public class FinAppIT {
 
-  private static SpannerDaoInterface JDBCDao;
-  private static SpannerDaoInterface JavaDao;
-  private static DatabaseClient databaseClient;
+  private SpannerDaoInterface spannerDao;
+  private DatabaseClient databaseClient;
+  private static Database db;
 
-  static {
-    // set all IDs according to emulator setup
-    String spannerProjectId = "test-project";
-    String spannerInstanceId = "test-instance";
-    String spannerDatabaseId = "test-database";
-    JDBCDao = new SpannerDaoJDBCImpl(spannerProjectId, spannerInstanceId, spannerDatabaseId);
-    SpannerOptions spannerOptions = SpannerOptions.getDefaultInstance();
-    Spanner spanner = spannerOptions.toBuilder().build().getService();
-    databaseClient =
-        spanner.getDatabaseClient(
-            DatabaseId.of(spannerProjectId, spannerInstanceId, spannerDatabaseId));
-    JavaDao = new SpannerDaoImpl(databaseClient);
+  @ClassRule public static IntegrationTestEnv env = new IntegrationTestEnv();
+
+  @BeforeClass
+  public static void setup() throws IOException {
+    RemoteSpannerHelper testHelper = env.getTestHelper();
+    db =
+        testHelper.createTestDatabase(
+            extractStatementsFromSDLFile("src/main/java/com/google/finapp/schema.sdl"));
   }
 
-  private SpannerDaoInterface getImpl(String impl) {
-    if (impl.equals("java client")) {
-      return JavaDao;
+  @Before
+  public void setupSpannerDao() {
+    RemoteSpannerHelper testHelper = env.getTestHelper();
+    databaseClient = testHelper.getDatabaseClient(db);
+    if (System.getProperty("SPANNER_USE_JDBC") == null
+        || System.getProperty("SPANNER_USE_JDBC").equalsIgnoreCase("false")) {
+      spannerDao = new SpannerDaoImpl(databaseClient);
     } else {
-      return JDBCDao;
+      spannerDao =
+          new SpannerDaoJDBCImpl(
+              testHelper.getOptions().getProjectId(),
+              testHelper.getInstanceId().getInstance(),
+              db.getId().getDatabase());
     }
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = {"java client", "jdbc"})
-  public void createAccountTest(String impl) throws SpannerDaoException {
-    SpannerDaoInterface spannerDao = getImpl(impl);
+  private static List<String> extractStatementsFromSDLFile(String filename)
+      throws FileNotFoundException {
+    File file = new File(filename);
+    BufferedReader reader = new BufferedReader(new FileReader(file));
+    StringBuilder builder = new StringBuilder();
+    try (Scanner scanner = new Scanner(reader)) {
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        if (!line.startsWith("--")) { // ignore comments
+          builder.append(line);
+        }
+      }
+    }
+    return List.of(builder.toString().split(";")); // separate into individual statements
+  }
+
+  @AfterClass
+  public static void tearDown() {
+    db.drop();
+  }
+
+  @Test
+  public void createAccount_createsSingleValidAccount() throws Exception {
     ByteArray accountId = UuidConverter.getBytesFromUuid(UUID.randomUUID());
+    BigDecimal amount = new BigDecimal(2);
     spannerDao.createAccount(
         accountId,
         AccountType.UNSPECIFIED_ACCOUNT_TYPE /* = 0*/,
         AccountStatus.UNSPECIFIED_ACCOUNT_STATUS /* = 0*/,
-        new BigDecimal(2));
+        amount);
     try (ResultSet resultSet =
         databaseClient
             .singleUse()
@@ -77,11 +114,14 @@ public class FinAppIT {
                 "Account",
                 KeySet.newBuilder().addKey(Key.of(accountId)).build(),
                 Arrays.asList("AccountType", "AccountStatus", "Balance"))) {
+      int count = 0;
       while (resultSet.next()) {
-        assertEquals(0, resultSet.getLong(0));
-        assertEquals(0, resultSet.getLong(1));
-        assertEquals(new BigDecimal(2), resultSet.getBigDecimal(2));
+        assertThat(resultSet.getLong(0)).isEqualTo(0);
+        assertThat(resultSet.getLong(1)).isEqualTo(0);
+        assertThat(resultSet.getBigDecimal(2)).isEqualTo(amount);
+        count++;
       }
+      assertThat(count).isEqualTo(1);
     }
   }
 }
