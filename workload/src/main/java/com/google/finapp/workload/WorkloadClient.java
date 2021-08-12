ed2ba45a32544417ca93a7e2f221e4bcbd8e83a4
Nicholas Garcia <ncgarcia@mit.edu>
2021-08-12 22:18:06 +0000

    Set up basic files for workload

---
+++ workload/src/main/java/com/google/finapp/workload/WorkloadClient.java
@@ -0,0 +1,11 @@
+package com.google.finapp.workload;
+
+import io.grpc.*;
+
+public class WorkloadClient {
+
+  /** Construct client for accessing RouteGuide server using the existing channel. */
+  public WorkloadClient(String host, int port) {
+    FinAppGrpc.FinAppBlockingStub stub;
+  }
+}
