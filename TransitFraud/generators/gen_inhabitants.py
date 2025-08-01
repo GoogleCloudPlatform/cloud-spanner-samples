bc5930663152e1a740e9e1a7339f11249dc79300
Lucia Subatin <24896969+Lsubatin@users.noreply.github.com>
2025-08-01 13:30:57 -0400

    Some Codelab adaptation, directory cleanup and headers 🦄 (#100)
    * Adding banking sample for codelab 🦄
    
    * Adding banking sample for codelab 🦄
    
    * Adding banking sample for codelab 🦄
    
    * Update contributing.md
    
    * Update contributing.md
    
    * some tweaks in structure, adding headers 🐱
    
    ---------
    
    Co-authored-by: Lucia Subatin <daunicorn@google.com>

---
+++ TransitFraud/generators/gen_inhabitants.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from faker import Faker
+import csv
+
+fake = Faker()
+
+with open("../data/inhabitants.csv", "w") as csvfile:
+    fieldnames = ["id", "to_person"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for i in range(1000):
+        writer.writerow(
+            {
+                "id": fake.random_int(min=0, max=849),
+                "to_person": i,
+            }
+        )
